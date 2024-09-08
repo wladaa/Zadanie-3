cf9c08eb73c33abd11848de54b557a9ae282f2e9
wladaa <wladzia1306@gmail.com>
2024-09-08 09:48:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Person //Definition of the abstract class
+{
+    public string FirstName;
+    public string LastName;
+
+    public Person(string firstName, string lastName)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+
+}
+
+
+public class Student : Person //Student class inheriting from Preson
+{
+    public string School;
+
+    public Student(string firstName, string lastName, string school) : base(firstName, lastName)
+    {
+        this.School = school;
+    }
+
+    public void Study()
+    {
+        Console.WriteLine($"{FirstName} {LastName} is studying at {School}.");
+    }
+}
+
+public class Teacher : Person //Teacher class inheriting from Person
+{
+    public string Subject;
+
+    public Teacher(string firstName, string lastName, string subject) : base(firstName, lastName)
+    {
+        this.Subject = subject;
+    }
+
+    public void Teach()
+    {
+        Console.WriteLine($"{FirstName} {LastName} is teaching {Subject}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Student student = new Student("Jan", "Kowalski", "TEB");
+        student.Study();
+
+        Teacher teacher = new Teacher("Anna", "Nowak", "Mathematics");
+        teacher.Teach();
+    }
+}
+
+
